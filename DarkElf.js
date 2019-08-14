c214d56431a601bcb655ce49db4746a4230a4a15
Oleksii <mysterious240@gmail.com>
2019-08-14 16:50:38 +0300

    fff

---
+++ DarkElf.js
@@ -0,0 +1,17 @@
+import { Unit } from './Unit.js';
+
+export class DarkElf extends Unit {
+  health = 10;
+  minPower = 2;
+  maxPower = 4;
+  maxDistance = 1;
+
+  attack(target) {
+    if (Math.abs(target.x - this.x) < this.maxDistance) {
+      super.attack(target);
+    } else {
+      this.move(this.x + 1, this.y + 1);
+    }
+  }
+
+}
